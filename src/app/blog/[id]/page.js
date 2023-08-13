a85cded6a22fc4ed30b010c553bab14f2288cc08
Joy <mmehedihasanjoyv@gmail.com>
2023-08-14 05:04:18 +0600

    created the next project

---
+++ src/app/blog/[id]/page.js
@@ -0,0 +1,38 @@
+
+
+// export const dynamicParams = false;
+
+import getAllPosts from "@/lib/getAllPost";
+import getBlogPost from "@/lib/getBlogPost";
+
+export default async function SingleBlog({ params }) {
+    const id = params.id;
+
+    const postPromise = getBlogPost(id);
+
+    // request in parallel
+    // const [post, comments] = await Promise.all([postPromise, commentsPromise]);
+
+    // incremental data fetching
+    const post = await postPromise;
+
+    return (
+        <div>
+            <section>
+                <h1 className="font-bold text-lg">{post.title}</h1>
+
+                <div className="mt-5">{post.body}</div>
+
+          
+            </section>
+        </div>
+    );
+}
+
+export async function generateStaticParams() {
+    const posts = await getAllPosts();
+
+    return posts.map((post) => ({
+        id: "" + post.id,
+    }));
+}
